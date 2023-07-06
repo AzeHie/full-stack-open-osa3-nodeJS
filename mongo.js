5b959941ddb2cb5dc74d66f1f424e7c9600e7a55
AzeHie <109618459+AzeHie@users.noreply.github.com>
2023-07-06 15:13:42 +0300

    Added mongo.js file, exercise 3-12

---
+++ mongo.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://AZE:${password}@full-stack-open.m1dmdbf.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(process.argv);
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length < 4) {
+  Person.find({}).then(res => {
+    console.log("phonebook:");
+    res.forEach(person => {
+      console.log(person);
+    });
+    mongoose.connection.close();
+  }) 
+} else {
+  const name = process.argv[3];
+  const number = process.argv[4];
+
+  const person = new Person({
+    name: name,
+    number: number,
+  });
+
+  person.save().then((res) => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
